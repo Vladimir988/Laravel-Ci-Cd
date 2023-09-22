d7d7efda722b7d48f51f94474169979ecc8c2ccd
Vladimir988 <volodymyr.prokopets@betterme.world>
2023-09-22 20:54:48 +0300

    new commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,6 +6,6 @@ class TestController extends Controller
 {
     public function __invoke()
     {
-        return 'This is test controller return';
+        return 'This is test controller return 1111 new test';
     }
 }
